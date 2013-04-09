bd711973bf305131b5307343e208dbb2bce34f77
erik <erikecoologic@gmail.com>
2013-04-09 20:01:46 +1000

    table mvc (fixture store)

--- js/app.js
+++ js/app.js
@@ -3,3 +3,29 @@ var App = Ember.Application.create();
 App.Router.map(function(){
   this.resource('tables'); // /#/tables
 });
+
+App.TablesRoute = Ember.Route.extend({
+  model: function() {
+    return App.Table.find();
+  }
+});
+
+App.TablesController = Ember.ArrayController.extend({});
+
+App.Store = DS.Store.extend({
+  revision: 11,
+  adapter: 'DS.FixtureAdapter'
+});
+
+App.Table = DS.Model.extend({});
+App.Table.FIXTURES = [
+  {id: 1},
+  {id: 2},
+  {id: 3},
+  {id: 4},
+  {id: 5},
+  {id: 6}
+];
+
+
+
